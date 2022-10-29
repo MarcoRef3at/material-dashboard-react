6ad3cb01fe82ad3f5f5115a4536c5607f23be4e3
marcoref3at <marco_refaat@hotmail.com>
2022-10-30 00:12:43 +0200

    cleaning 2

--- src/layouts/monitor/index.js
+++ src/layouts/monitor/index.js
@@ -9,7 +9,6 @@ import MDTypography from "components/MDTypography";
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
 import DataTable from "examples/Tables/DataTable";
 
 //Material progress bar
